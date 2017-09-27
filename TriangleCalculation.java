919960851d0afd181cf939b927830f9c76c68251
pelenovski <31411489+pelenovski@users.noreply.github.com>
2017-09-27 10:24:06 -0500

    Chapter 3

--- TriangleCalculation.java
+++ TriangleCalculation.java
@@ -19,12 +19,25 @@ public class TriangleCalculation
 		s2 = keyboard.nextInt();
 		s3 = keyboard.nextInt();
 
-			//if
-
-
-
-
-
+			if ((s1 > 0) && (s2 > 0) && (s3 > 0))
+				{
+					if (s1 != s2)
+						{
+							if (s2 != s3)
+							System.out.println("The triangle is scalene.");
+							if (s2 == s3)
+							System.out.println("The triangle is isoceles.");
+						}
+					if (s1 == s2)
+						{
+							if (s2 != s3)
+							System.out.println("The triangle is isoceles.");
+							if (s2 == s3)
+							System.out.println("The triangle is equilateral");
+						}
+				}
+			else
+			System.out.println("Error! Sides can not equal 0.");
 
 
 	}
