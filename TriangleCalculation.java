58cacf4ef6b6fd9f0a609a9f0a6eb38a384439bf
pelenovski <31411489+pelenovski@users.noreply.github.com>
2017-09-27 10:15:37 -0500

    Chapter 3

--- TriangleCalculation.java
+++ TriangleCalculation.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Pane Elenovski
 //9/22/2017
 //Triangle Program
@@ -12,27 +11,15 @@ public class TriangleCalculation
 	{
 		//Initiates the variables needed in program
 		int s1,s2,s3;
-		char choice;
 		Scanner keyboard = new Scanner(System.in);
 
-		{
-			System.out.print("Please enter the lengths of side 1, side 2 and side 3");
+		System.out.print("Please enter the lengths of side 1, side 2 and side 3");
 
-			s1 = keyboard.nextInt();
-			s2 = keyboard.nextInt();
-			s3 = keyboard.nextInt();
+		s1 = keyboard.nextInt();
+		s2 = keyboard.nextInt();
+		s3 = keyboard.nextInt();
 
-			if
-
-
-		}
-
-
-
-
-
-
-
+			//if
 
 
 
@@ -42,52 +29,3 @@ public class TriangleCalculation
 
 	}
 }
-=======
-//Pane Elenovski
-//9/22/2017
-//Triangle Program
-//Enter sides and informs user on triangle type
-
-import java.util.Scanner;
-
-public class TriangleCalculation
-{
-	public static void main(String []args)
-	{
-		//Initiates the variables needed in program
-		int s1,s2,s3;
-		char choice;
-		Scanner keyboard = new Scanner(System.in);
-
-		//Asks user for option between sides or angles
-		System.out.print("Would you like to enter the angles or the sides (A for angles, S for sides)");
-
-		choice = Scanner.next().	CharAt();
-
-		if (choice == 'S')
-		{
-			System.out.print("Please enter the lengths of side 1, side 2 and side 3");
-
-			s1 = keyboard.nextInt();
-			s2 = keyboard.nextInt();
-			s3 = keyboard.nextInt();
-
-
-		}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	}
-}
->>>>>>> 55d2599386d70b0bd0a84b67e3c2a55fade4ed76
